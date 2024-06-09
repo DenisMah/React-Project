b72f4f602c94d1495528ef24b8bd3f7a0d71cbe1
DenisMah <deenkom37@gmail.com>
2024-06-09 09:57:59 +0200

    Adding more routing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contactme" element={<Contact />} />
         </Routes>
       </div>
     </Router>
--- src/Navbar.js
+++ src/Navbar.js
@@ -25,7 +25,7 @@ function Navbar() {
             </Link>
           </li>
           <li>
-            <Link to="/contact" className="anchor-navbar">
+            <Link to="/contactme" className="anchor-navbar">
               Contact
             </Link>
           </li>
